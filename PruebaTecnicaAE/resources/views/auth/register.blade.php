a8a29134de14ec6df42ab6e06403e30127cf2c49
gdvs1997 <gdvs1997@gmail.com>
2024-12-15 01:36:11 -0600

    Crear Procfile para Heroku

--- PruebaTecnicaAE/resources/views/auth/register.blade.php
+++ PruebaTecnicaAE/resources/views/auth/register.blade.php
@@ -8,11 +8,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../public/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{ url('/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="../public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="{{ url('/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../public/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{ url('/dist/css/adminlte.min.css') }}">
 </head>
 <body class="hold-transition register-page">
 <div class="register-box">
@@ -101,10 +101,10 @@
 <!-- /.register-box -->
 
 <!-- jQuery -->
-<script src="../public/plugins/jquery/jquery.min.js"></script>
+<script src="{{ url('/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ url('/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="../public/dist/js/adminlte.min.js"></script>
+<script src="{{ url('/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
--- PruebaTecnicaAE/routes/web.php
+++ PruebaTecnicaAE/routes/web.php
@@ -23,9 +23,9 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index')->middleware('auth');
-Route::get('/usuarios', [App\Http\Controllers\UsuarioController::class, 'index'])->name('usuarios.index')->middleware('auth');
+//Route::get('/usuarios', [App\Http\Controllers\UsuarioController::class, 'index'])->name('usuarios.index')->middleware('auth');
 
 Route::resource('productos', App\Http\Controllers\ProductoController::class)->middleware('auth');
 Route::resource('categorias', App\Http\Controllers\CategoriaController::class)->middleware('auth');
